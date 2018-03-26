15048fbd7ebcae4a392cac75ffbd4b15d0f6e5d8
kmng1 <kmng@ieee.org>
2018-03-26 15:15:29 +0800

    Sec 5 Test - Lesson 95 Session and User factory tests

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,6 +1,9 @@
 const puppeteer = require('puppeteer');
+const sessionFactory = require('./factories/sessionFactory');
+const userFactory = require('./factories/userFactory');
 
 let browser, page;
+const ASYNC_TIMEOUT = 30000;
 
 beforeEach(async () => {
   browser = await puppeteer.launch({
@@ -23,41 +26,30 @@ test('Adds two numbers', () => {
 test('The header has the correct text', async () => {
   const text = await page.$eval('a.brand-logo', el => el.innerHTML);
   expect(text).toEqual('Blogster');
-}, 20000);
+}, ASYNC_TIMEOUT);
 
 test('Clicking login starts oauth flow', async () => {
   await page.click('.right a');
   const url = await page.url();
   expect(url).toMatch(/accounts\.google\.com/);
-}, 20000);
+}, ASYNC_TIMEOUT);
 
 test('The header has the correct text', async () => {
   const text = await page.$eval('a.brand-logo', el => el.innerHTML);
   expect(text).toEqual('Blogster');
-}, 20000);
+}, ASYNC_TIMEOUT);
 
 // test.only('When signed in, show logout button', async () => {
 test('When signed in, show logout button', async () => {
-  const id = '5ab6850e7432224614283fb1';
-  const Buffer = require('safe-buffer').Buffer;
-  const sessionObject = {
-    passport: {
-      user: id
-    }
-  };
-  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString('base64');
-  const Keygrip = require('keygrip');
-  const keys = require('../config/keys');
-  const keygrip = new Keygrip([keys.cookieKey]);
-  const sig = keygrip.sign('session=' + sessionString);
-  // console.log(sessionString, sig);
+  const user = await userFactory();
+  const { session, sig } = sessionFactory(user);
 
-  await page.setCookie({ name: 'session', value: sessionString});
+  await page.setCookie({ name: 'session', value: session});
   await page.setCookie({ name: 'session.sig', value: sig});
   await page.goto('localhost:3000');
   await page.waitFor('a[href="/auth/logout"]');
 
   const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
   expect(text).toEqual('Logout');
-}, 20000);
+}, ASYNC_TIMEOUT);
 
